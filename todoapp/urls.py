3f0c6bf6993b9473d8e10ff0b3e844fde27cf9da
dprajktaraje765@gmail.com <dprajktaraaje765@gmail.com>
2021-03-20 12:39:32 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from todoapp import views
+
+urlpatterns = [
+    path('', views.apiOverview, name="api-overview"),
+    path('task-list/',views.taskList,name='task-list'),
+    path('task-detail/<str:pk>/',views.taskDetail,name='task-Detail'),
+    path('task-create/',views.taskCreate,name='task-create'),
+    path('task-update/<str:pk>/',views.taskUpdate,name='task-Update'),
+    path('task-delete/<str:pk>/',views.taskDelete,name='task-Delete'),
+]
